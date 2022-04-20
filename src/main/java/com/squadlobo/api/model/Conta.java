30a6b4cacf6522147352724a4b2fe25858236851
Roberto Brito <roberto.brito.oliveira@hotmail.com>
2022-04-20 16:02:54 -0300

    branch dev

--- src/main/java/com/squadlobo/api/model/Conta.java
+++ src/main/java/com/squadlobo/api/model/Conta.java
@@ -11,7 +11,7 @@ import javax.persistence.OneToOne;
 @Entity
 public class Conta implements Serializable {
 
-	//private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
 	@Id
 	private String numeroConta;
